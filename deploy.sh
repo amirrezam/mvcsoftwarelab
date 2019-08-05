c4f75b8694107e70ed1774efd9574888719695a8
Amirreza Mozayani <amirrezamozayani@gmail.com>
2019-08-05 15:36:13 +0430

    change travis

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+password=amirrezamozayani@gmail.com
+username=94110169
+ant make
+sshpass -p "$password" scp -o StrictHostKeyChecking=no mvc.jar $username@198.144.184.34:/home/$username/
+sshpass -p "$password" ssh $username@198.144.184.34 pkill java
+sshpass -p "$password" ssh $username@198.144.184.34 nohup java -jar /home/$username/mvc.jar &
