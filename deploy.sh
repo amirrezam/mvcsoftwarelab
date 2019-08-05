7bfdb00a7e2418adb5e4a900ed1b005afe82fc32
Amirreza Mozayani <amirrezamozayani@gmail.com>
2019-08-05 15:59:55 +0430

    ci/cd final

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@ password=amirrezamozayani@gmail.com
 username=94110169
 ant make
 sshpass -p "$password" scp -o StrictHostKeyChecking=no mvc.jar $username@198.144.184.34:/home/$username/
-sshpass -p "$password" scp -o StrictHostKeyChecking=no -r static $username@198.144.184.34:/home/$username/static
+sshpass -p "$password" scp -o StrictHostKeyChecking=no -r static $username@198.144.184.34:/home/$username/
 sshpass -p "$password" ssh $username@198.144.184.34 pkill java
 sshpass -p "$password" ssh $username@198.144.184.34 nohup java -jar /home/$username/mvc.jar &
